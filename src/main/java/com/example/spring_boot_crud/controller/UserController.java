57642f3a6e25de215492a9a3cf463789f2ea650d
korpusoff91 <p.korpusoff@yandex.ru>
2022-07-26 00:36:04 +0300

    Удаление юзера через @DeleteMapping

--- src/main/java/com/example/spring_boot_crud/controller/UserController.java
+++ src/main/java/com/example/spring_boot_crud/controller/UserController.java
@@ -5,6 +5,7 @@ import com.example.spring_boot_crud.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -39,7 +40,7 @@ public class UserController {
         return "redirect:/";
     }
 
-    @GetMapping("/user-delete/{id}")
+    @DeleteMapping("/user-delete/{id}")
     public String removeUser(@PathVariable("id") Long id) {
         userService.deleteById(id);
         return "redirect:/";
